a34cb78f13281a513baca33afc46668989f6f7c9
Hamza Ouggadi <45943796+HamzaOuggadi@users.noreply.github.com>
2025-06-08 17:13:02 +0100

    Initial commit

---
+++ src/main/java/net/noxe/retroarchivebe/RetroArchiveBeApplication.java
@@ -0,0 +1,13 @@
+package net.noxe.retroarchivebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroArchiveBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetroArchiveBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/noxe/retroarchivebe/RetroArchiveBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.noxe.retroarchivebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroArchiveBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
